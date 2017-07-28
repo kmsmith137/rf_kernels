9fc618133c59dafe09482d749469df4fae7cc507
mburhanpurkar <maya.burhanpurkar@gmail.com>
2017-07-28 18:07:29 -0400

    An attempt at integrating the fast random number generator

--- online_mask_filler.cpp
+++ online_mask_filler.cpp
@@ -20,22 +20,23 @@ inline bool get_v1(const float *intensity, const float *weights, float &v1)
     {
         // I assume this is okay for checking whether the weight is 0?
         if (weights[i] < 1e-7)
-	  ++zerocount;
-        vsum += intensity[i] * intensity[i] * weights[i];
-        wsum += weights[i];
+	    ++zerocount;
+	vsum += intensity[i] * intensity[i] * weights[i];
+	wsum += weights[i];
     }
 
     wsum = max(wsum, 1.0f);
-
+    
     // Check whether enough valid values were passed
     if (zerocount >= 23.9)
-    {  
-	v1 = 0;
-        return false;
+    {
+        v1 = 0;
+	return false;
     }
     v1 = vsum / wsum;
     return true;
 }
+
 
 
 // Based on Maya code in rf_pipelines (scalar_mask_fill() in online_mask_filler.cpp)
@@ -54,11 +55,9 @@ inline bool get_v1(const float *intensity, const float *weights, float &v1)
 //     random intensity has variance (3 * running_weight^2 * running_variance).
 //
 //   - See "KMS" below for two more minor changes...
-
-
 void online_mask_fill(const online_mask_filler_params &params, int nfreq, int nt_chunk, int stride,
-		      float *intensity, const float *weights, float *running_var, float *running_weights, 
-		      std::mt19937 &rng)
+		      float *intensity, const float *weights, float *running_var, float *running_weights,
+		      xorshift_plus &rand_x)
 {
     const int v1_chunk = params.v1_chunk;
     const float w_clamp = params.w_clamp;
@@ -67,61 +66,71 @@ void online_mask_fill(const online_mask_filler_params &params, int nfreq, int nt
     const float var_clamp_mult = params.var_clamp_mult;
     const float w_cutoff = params.w_cutoff;
 
+    float rn[8]; // holds random numbers for mask filling
+    
     // outer looop over frequency channels
-    for (int ifreq = 0; ifreq < nfreq; ifreq++) {
-	// (running_variance, running_weights) for this frequency channel
-	float rv = running_var[ifreq];
+    for (int ifreq = 0; ifreq < nfreq; ifreq++)
+    {
+        // (running_variance, running_weights) for this frequency channel
+        float rv = running_var[ifreq];
 	float rw = running_weights[ifreq];
 	float v1;
-
+	
 	// middle loop over v1_chunks
-	for (int ichunk = 0; ichunk < nt_chunk; ichunk += v1_chunk) {
+	for (int ichunk = 0; ichunk < nt_chunk; ichunk += v1_chunk)
+	{
 	    // (intensity, weights) pointers for this v1_chunk
 	    float *iacc = &intensity[ifreq*stride + ichunk];
-            const float *wacc = &weights[ifreq*stride + ichunk];
-
-	    if (!get_v1(iacc, wacc, v1)) {
-		// For an unsuccessful v1, we decrease the weight if possible. We do not modify the running variance
-		rw = max(0.0f, rw - w_clamp);
+	    const float *wacc = &weights[ifreq*stride + ichunk];
+	    
+	    if (!get_v1(iacc, wacc, v1))
+	    {
+	        // For an unsuccessful v1, we decrease the weight if possible. We do not modify the running variance
+	        rw = max(0.0f, rw - w_clamp);
 	    }
-	    else if (rw <= 0.0f) {
-		// KMS: changed logic slightly here!  If the v1 is successful but the running_weight is zero,
-		// we set the running_variance equal to the value of 'v1' (ignoring the old value of running_variance),
-		// rather than slowly transitioning from the old variance estimate to the new one.
-		rv = v1;
+	    else if (rw <= 0.0f) 
+	    {
+	        // KMS: changed logic slightly here!  If the v1 is successful but the running_weight is zero,
+	        // we set the running_variance equal to the value of 'v1' (ignoring the old value of running_variance),
+	        // rather than slowly transitioning from the old variance estimate to the new one.
+	        rv = v1;
 		rw = w_clamp;
-	    }
-	    else {
-		// If the v1 was succesful, try to increase the weight, if possible
-		// KMS: changed max weight from 2.0f to 1.0f here, since taking max_weight=2 was confusing one of my
-		// bonsai unit tests, and max_weight=2 was just catering to a bug of mine in rf_pipelines anyway!
-		rw = min(1.0f, rw + w_clamp);
+	    } 
+	    else 
+	    {
+	        // If the v1 was succesful, try to increase the weight, if possible
+	        // KMS: changed max weight from 2.0f to 1.0f here, since taking max_weight=2 was confusing one of my
+	        // bonsai unit tests, and max_weight=2 was just catering to a bug of mine in rf_pipelines anyway!
+	        rw = min(1.0f, rw + w_clamp);
 
 		// Then, restrict the change in variance estimate defined by the clamp parameters
 		v1 = (1 - var_weight) * rv + var_weight * v1;
 		v1 = min(v1, rv + var_clamp_add + rv*var_clamp_mult);
-                v1 = max(v1, rv - var_clamp_add - rv*var_clamp_mult);
+		v1 = max(v1, rv - var_clamp_add - rv*var_clamp_mult);
 		rv = v1;
 	    }
-
+	    
 	    // Scaling factor for random numbers.
 	    // KMS note factor of the running_weight here!
 	    float scale = rw * sqrt(3*rv);
-
-	    for (int i = 0; i < v1_chunk; i++) {
+	    
+	    for (int i = 0; i < v1_chunk; i++)
+	    {
+	        if (i % 8 == 0)
+	  	rand_x.gen_floats(rn);
+		iacc[i] = (wacc[i] < w_cutoff) ? rw * iacc[i] : rn[i % 8] * scale;
 		// r = uniform random number in [-scale,scale]
-		float r = std::uniform_real_distribution<float>(-scale,scale)(rng);
-
+		//float r = std::uniform_real_distribution<float>(-scale,scale)(rng);
+		
 		// if unmasked, then apply running_weight
 		// if masked, then set intensity to random.
-		iacc[i] = (wacc[i] >= w_cutoff) ? (rw * iacc[i]) : r;
+		//iacc[i] = (wacc[i] >= w_cutoff) ? (rw * iacc[i]) : r;
 	    }
 	}
 
 	running_var[ifreq] = rv;
 	running_weights[ifreq] = rw;
-    }    
+    }
 }
-
 
 }  // namespace rf_kernels
--- rf_kernels.hpp
+++ rf_kernels.hpp
@@ -13,6 +13,54 @@ namespace rf_kernels {
 #endif
 
 
+// -------------------------------------------------------------------------------------------------
+// Class for random number generation
+// Generates eight random 32-bit floats using a vectorized implementation of xorshift+
+// between (-1, 1)
+struct xorshift_plus
+{
+  std::vector<uint64_t> seeds;
+  
+  // Constructor for specifying seeds -- debug/unit test constructor
+  xorshift_plus(uint64_t _s0, uint64_t _s1, 
+		uint64_t _s2, uint64_t _s3, 
+		uint64_t _s4, uint64_t _s5, 
+		uint64_t _s6, uint64_t _s7)
+    : seeds{_s0, _s1, _s2, _s3, _s4, _s5, _s6, _s7} {};
+
+  
+  // Initialize with random_device -- for production
+  xorshift_plus() 
+  {
+      std::random_device rd;
+      seeds = {rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
+  };
+
+  
+  inline void gen_floats(float *rn)
+  {
+    // Generates 8 random floats and stores in rn
+    for (int i=0; i<8; i+=2)
+      {
+	uint64_t x = seeds[i];
+	uint64_t y = seeds[i+1];
+
+	seeds[i] = y;
+	x ^= (x << 23);
+	seeds[i+1] = x ^ y ^ (x >> 17) ^ (y >> 26);
+
+	uint64_t tmp = seeds[i+1] + y;
+	uint32_t tmp0 = tmp; // low 32 bits
+	uint32_t tmp1 = tmp >> 32; // high 32
+
+	rn[i] = float(int32_t(tmp0)) * 4.6566129e-10;
+	rn[i+1] = float(int32_t(tmp1)) * 4.6566129e-10;
+      }
+  }
+};
+
+
+
 struct online_mask_filler_params {
     int v1_chunk = 32;
     float var_weight = 2.0e-3;      // running_variance decay constant
@@ -28,7 +76,7 @@ struct online_mask_filler_params {
 
 extern void online_mask_fill(const online_mask_filler_params &params, int nfreq, int nt_chunk, int stride,
 			     float *intensity, const float *weights, float *running_var, float *running_weights, 
-			     std::mt19937 &rng);
+			     xorshift_plus &rand_x);
 
 
 }  // namespace rf_kernels
