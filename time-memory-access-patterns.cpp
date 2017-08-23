210cce9663cd129dd3a5261c9bac3796e8ed4c58
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-08-23 13:49:07 -0400

    time_update_cols

--- time-memory-access-patterns.cpp
+++ time-memory-access-patterns.cpp
@@ -37,6 +37,17 @@ inline void read_ntuple(simd_ntuple<float,S,N> &dst, const float *src, int strid
 }
 
 
+template<unsigned int N, typename enable_if<(N==0),int>::type = 0>
+inline void write_ntuple(float *dst, const simd_ntuple<float,S,N> &src, int stride) { }
+
+template<unsigned int N, typename enable_if<(N>0),int>::type = 0>
+inline void write_ntuple(float *dst, const simd_ntuple<float,S,N> &src, int stride)
+{
+    write_ntuple(dst, src.v, stride);
+    simd_store(dst + (N-1)*stride, src.x);
+}
+
+
 // -------------------------------------------------------------------------------------------------
 
 
@@ -96,10 +107,62 @@ inline void read_cols(int nfreq, int nt_chunk, int stride, const float *src)
 }
 
 
+template<unsigned int N>
+inline void update_cols(int nfreq, int nt_chunk, int stride, float *arr)
+{
+    simd_t<float,S> one(1.0);
+	
+    for (int it = 0; it < nt_chunk; it += N*S) {
+	for (int ifreq = 0; ifreq < nfreq; ifreq++) {
+	    simd_ntuple<float,S,N> x;
+	    read_ntuple(x, arr + ifreq*stride + it, S);
+	    x += one;
+	    write_ntuple(arr + ifreq*stride + it, x, S);
+	}
+    }
+}
+
+
 struct memory_access_timing_thread : public kernel_timing_thread {
     memory_access_timing_thread(const shared_ptr<timing_thread_pool> &pool_, const kernel_timing_params &params_) :
 	kernel_timing_thread(pool_, params_)
     { }
+
+    template<unsigned int N>
+    inline void time_read_rows(const char *str)
+    {
+	this->start_timer();
+	for (int iter = 0; iter < niter; iter++)
+	    read_rows<N> (nfreq, nt_chunk, stride, intensity);
+	this->stop_timer2(str);
+    }
+
+    template<unsigned int N>
+    inline void time_read_rows_2arr(const char *str)
+    {
+	this->start_timer();
+	for (int iter = 0; iter < niter; iter++)
+	    read_rows_2arr<N> (nfreq, nt_chunk, stride, intensity, weights);
+	this->stop_timer2(str);
+    }
+
+    template<unsigned int N>
+    inline void time_read_cols(const char *str)
+    {
+	this->start_timer();
+	for (int iter = 0; iter < niter; iter++)
+	    read_cols<N> (nfreq, nt_chunk, stride, intensity);
+	this->stop_timer2(str);
+    }
+
+    template<unsigned int N>
+    inline void time_update_cols(const char *str)
+    {
+	this->start_timer();
+	for (int iter = 0; iter < niter; iter++)
+	    update_cols<N> (nfreq, nt_chunk, stride, intensity);
+	this->stop_timer2(str);
+    }
 
     virtual void thread_body() override 
     {
@@ -108,66 +171,24 @@ struct memory_access_timing_thread : public kernel_timing_thread {
 
 	this->allocate();
 
-	// read_rows<N>
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_rows<1> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_rows<1>");
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_rows<2> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_rows<2>");
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_rows<4> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_rows<4>");
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_rows<8> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_rows<8>");
+	this->time_read_rows<1> ("read_rows<1>");
+	this->time_read_rows<2> ("read_rows<2>");
+	this->time_read_rows<4> ("read_rows<4>");
+	this->time_read_rows<8> ("read_rows<8>");
 
-	// read_rows_2arr<N>
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_rows_2arr<1> (nfreq, nt_chunk, stride, intensity, weights);
-	this->stop_timer2("read_rows_2arr<1>");
+	this->time_read_rows_2arr<1> ("read_rows_2arr<1>");	
+	this->time_read_rows_2arr<2> ("read_rows_2arr<2>");	
+	this->time_read_rows_2arr<4> ("read_rows_2arr<4>");	
 
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_rows_2arr<2> (nfreq, nt_chunk, stride, intensity, weights);
-	this->stop_timer2("read_rows_2arr<2>");
+	this->time_read_cols<1> ("read_cols<1>");
+	this->time_read_cols<2> ("read_cols<2>");
+	this->time_read_cols<4> ("read_cols<4>");
+	this->time_read_cols<8> ("read_cols<8>");
 
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_rows_2arr<4> (nfreq, nt_chunk, stride, intensity, weights);
-	this->stop_timer2("read_rows_2arr<4>");
-
-	// read_cols<N>
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_cols<1> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_cols<1>");
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_cols<2> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_cols<2>");
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_cols<4> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_cols<4>");
-	
-	this->start_timer();
-	for (int iter = 0; iter < niter; iter++)
-	    read_cols<8> (nfreq, nt_chunk, stride, intensity);
-	this->stop_timer2("read_cols<8>");
+	this->time_update_cols<1> ("update_cols<1>");
+	this->time_update_cols<2> ("update_cols<2>");
+	this->time_update_cols<4> ("update_cols<4>");
+	this->time_update_cols<8> ("update_cols<8>");
     }
 };
 
