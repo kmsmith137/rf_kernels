985036339c611e8ee41df0bd1f3899310c2093d7
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-08-31 16:24:35 -0400

    new downsampling kernels pass unit tests, but could use a little cleanup/optimization

--- test-downsample.cpp
+++ test-downsample.cpp
@@ -47,7 +47,7 @@ static void reference_wi_downsample(int nfreq_out, int nt_out, float *out_i, flo
 static void test_wi_downsample(std::mt19937 &rng, int nfreq_out, int nt_out, int ostride, int istride, int Df, int Dt)
 {
     int nfreq_in = nfreq_out * Df;
-    vector<float> in_i = uniform_randvec(rng, nfreq_in * istride, 0.0, 1.0);
+    vector<float> in_i = rf_kernels::uniform_randvec(rng, nfreq_in * istride, 0.0, 1.0);
     vector<float> in_w = vector<float> (nfreq_in * istride, 0.0);
 
     // Low value chosen to expose corner cases.
@@ -55,14 +55,33 @@ static void test_wi_downsample(std::mt19937 &rng, int nfreq_out, int nt_out, int
     for (size_t i = 0; i < in_w.size(); i++)
 	in_w[i] = (uniform_rand(rng) < pnonzero) ? uniform_rand(rng) : 0.0;
 
-    vector<float> out_i1 = uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
-    vector<float> out_i2 = uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
-    vector<float> out_w1 = uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
-    vector<float> out_w2 = uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
+    vector<float> out_i1 = rf_kernels::uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
+    vector<float> out_i2 = rf_kernels::uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
+    vector<float> out_w1 = rf_kernels::uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
+    vector<float> out_w2 = rf_kernels::uniform_randvec(rng, nfreq_out * ostride, 0.0, 1.0);
 
     wi_downsampler ds(Df, Dt);
     ds.downsample(nfreq_out, nt_out, &out_i1[0], &out_w1[0], ostride, &in_i[0], &in_w[0], istride);
     reference_wi_downsample(nfreq_out, nt_out, &out_i2[0], &out_w2[0], ostride, &in_i[0], &in_w[0], istride, Df, Dt);					     
+
+#if 0
+    // Uncomment for an avalanche of debugging output!
+    for (int ifreq_out = 0; ifreq_out < nfreq_out; ifreq_out++) {
+	for (int it_out = 0; it_out < nt_out; it_out++) {
+	    cout << "ifreq_out=" << ifreq_out << ", it_out=" << it_out
+		 << ": fast=(" << out_i1[ifreq_out*ostride+it_out] << "," << out_w1[ifreq_out*ostride+it_out] 
+		 << "), slow=(" << out_i2[ifreq_out*ostride+it_out] << "," << out_w2[ifreq_out*ostride+it_out] 
+		 << ")\n";
+
+	    for (int ifreq_in = ifreq_out*Df; ifreq_in < (ifreq_out+1)*Df; ifreq_in++) {
+		cout << "    ";
+		for (int it_in = it_out*Dt; it_in < (it_out+1)*Dt; it_in++)
+		    cout << " (" << in_i[ifreq_in*istride+it_in] << "," << in_w[ifreq_in*istride+it_in] << ")";
+		cout << endl;
+	    }
+	}
+    }
+#endif
     
     for (int ifreq_out = 0; ifreq_out < nfreq_out; ifreq_out++) {
 	for (int it_out = 0; it_out < nt_out; it_out++) {
@@ -76,14 +95,16 @@ static void test_wi_downsample(std::mt19937 &rng, int nfreq_out, int nt_out, int
 
 static void test_wi_downsample(std::mt19937 &rng)
 {
-    for (int iter = 0; iter < 1000; iter++) {
-	int Df = 1 << randint(rng, 0, 5);
-	int Dt = 1 << randint(rng, 0, 5);
+    const int niter = 1000;
+
+    for (int iter = 0; iter < niter; iter++) {
+	int Df = 1 << randint(rng, 0, 7);
+	int Dt = 1 << randint(rng, 0, 7);
 	
 	int nfreq_out = randint(rng, 1, 17);
 	int nt_out = 8 * randint(rng, 1, 17);
-	int ostride = randint(rng, nt_out, 2*nt_out);
-	int istride = randint(rng, Dt*nt_out, 2*Dt*nt_out);
+	int ostride = nt_out;
+	int istride = Dt*nt_out;
 	
 	test_wi_downsample(rng, nfreq_out, nt_out, ostride, istride, Df, Dt);
     }
