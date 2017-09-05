9868465170b751b66d5203592c93d7b651105f0d
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-09-05 08:26:08 -0400

    intensity_clipper(AXIS_NONE,1,1)

--- rf_kernels/intensity_clipper_internals.hpp
+++ rf_kernels/intensity_clipper_internals.hpp
@@ -111,7 +111,7 @@ inline void kernel_intensity_clipper_faxis(const intensity_clipper *ic, const T 
 }
 
 
-template<typename T, int S, int DfX, int DtX, bool TwoPass>
+template<typename T, int S, int DfX, int DtX, bool TwoPass, typename std::enable_if<((DfX>1)||(DtX>1)),int>::type = 0>
 inline void kernel_intensity_clipper_naxis(const intensity_clipper *ic, const T *in_i, T *in_w, int stride)
 {
     // For upsampler.  Note std::min() can't be used in a constexpr!
@@ -231,6 +231,41 @@ inline void kernel_intensity_clipper_faxis(const intensity_clipper *ic, const T 
 
 	for (int ifreq = 0; ifreq < nfreq; ifreq++) {
 	    simd_t<T,S> mask = buf.get_mask(mean, thresh, ifreq);
+
+	    float *p = in_w + ifreq*stride + it;
+	    simd_t<T,S> w = simd_helpers::simd_load<T,S> (p);
+	    simd_helpers::simd_store(p, w & mask);
+	}
+    }
+}
+
+template<typename T, int S, int DfX, int DtX, bool TwoPass, typename std::enable_if<((DfX==1)&&(DtX==1)),int>::type = 0>
+inline void kernel_intensity_clipper_naxis(const intensity_clipper *ic, const T *in_i, T *in_w, int stride)
+{
+    constexpr bool Hflag = true;
+
+    const int nfreq = ic->nfreq;
+    const int nt = ic->nt_chunk;
+    const int niter = ic->niter;
+    const simd_t<T,S> sigma(ic->sigma);
+    const simd_t<T,S> iter_sigma(ic->iter_sigma);
+    
+    _wrms_buf_strided<T,S> buf(in_i, in_w, nfreq, nt, stride);
+    _wrms_first_pass<T,S,Hflag,TwoPass> fp;
+
+    simd_t<T,S> mean, var;
+    fp.run(buf, mean, var);
+    
+    // Note iter_sigma here (not sigma)
+    _wrms_iterate<Hflag> (buf, mean, var, niter-1, iter_sigma);
+	
+    // Note sigma here (not iter_sigma)
+    simd_t<T,S> thresh = sigma * var.sqrt();
+    
+    // Mask rows
+    for (int ifreq = 0; ifreq < nfreq; ifreq++) {
+	for (int it = 0; it < nt; it += S) {
+	    simd_t<T,S> mask = buf.get_mask(mean, thresh, ifreq, it);
 
 	    float *p = in_w + ifreq*stride + it;
 	    simd_t<T,S> w = simd_helpers::simd_load<T,S> (p);
